e17b7800e88f5f86d2f0d9918aeeff572f6603ab
p-vickerman <patrick@vickerman.net>
2022-06-15 19:49:45 -0400

    adding completed file

--- main.js
+++ main.js
@@ -2,6 +2,51 @@
 // │ Your code here! │	
 // └─────────────────┘
 
+function isAdmin (user){
+    if (user.userRole === 'ADMIN'){
+        return true
+    } else {
+        return false
+    }  
+}
+
+function getEmail (user){
+    let email = "@codeimmersives.com"
+    user = user.firstName + "." + user.lastName + email
+   
+    return user.toLowerCase()
+}
+
+function getPlaylistLength (music){
+    return music.songs.length
+}
+
+function getHardestHomework(homework){
+    if(homework.length === 0){
+        return ''}
+    let lowest = homework[0].averageScore;
+    let lowestName = homework[0].name;
+
+    for (let i = 0; i < homework.length; i++){
+        if(homework[i].averageScore < lowest){
+            lowest = homework[i].averageScore;
+            lowestName = homework[i].name;
+        }
+    }
+    return lowestName
+}
+
+function createPhonebook (name, number){
+    let object = {};
+    for(let i = 0; i < name.length; i++){
+        object[name[i]] = number[i];
+    }
+    return object
+    
+
+}
+
+
 
 
 // ┌─────────────────────────────────────┐
